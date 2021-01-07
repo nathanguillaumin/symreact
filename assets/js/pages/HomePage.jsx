80ecf307d4a09cbd50a2c52146c5a72c2d486176
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2021-01-07 16:27:45 +0100

    changes wording for home

--- assets/js/pages/HomePage.jsx
+++ assets/js/pages/HomePage.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const HomePage = (props) => {
-  return (<div className='jumbotron'>
-    <h1 className='display-3'>Hello, world!</h1>
-    <p className='lead'>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-    <hr className='my-4' />
-    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-    <p className='lead'>
-      <a className='btn btn-primary btn-lg' href='#' role='button'>Learn more</a>
-    </p>
-  </div>);
+  return (
+    <div className="jumbotron">
+      <h1 className="display-3">Welcome to the SymReact app!</h1>
+      <p className="lead">
+        The technologies used to build this app are React.js and Symfony.
+        <br />
+        I encourage you to create an account, you will then be able to display a
+        list of customers and a list of their belongings invoices.
+        <br />
+        If you need any help, please let me know.
+      </p>
+      <hr className="my-4" />
+      <p>Nathan Guillaumin</p>
+    </div>
+  );
 };
 
 export default HomePage;
